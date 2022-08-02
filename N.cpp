b2832f44d83a7b281cc3be18481c4b8214dcc43d
Nourhan-Atef-Hossam-El-Deen <91796430+Nourhaan-Atef@users.noreply.github.com>
2022-08-02 13:57:28 +0200

    N Problem

---
+++ N.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,x;
+	string s;
+	cin>>s>>n;
+	for(int i=1;i<=n;i++){
+	   cin>>x;
+	   for(int j=1;j<=x;j++){
+	   	cout<<s;
+	   }
+	   cout<<endl;
+	}
+}
